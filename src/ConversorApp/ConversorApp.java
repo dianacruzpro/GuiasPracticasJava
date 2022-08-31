4cfc215a834ea8696bc54a2623ab331cb52ea8d0
Diana Cruz <saravcruz1501@gmail.com>
2022-08-30 19:17:11 -0600

    Adding practice

--- src/ConversorApp/ConversorApp.java
+++ src/ConversorApp/ConversorApp.java
@@ -16,29 +16,39 @@ public class ConversorApp {
         double temperatura;
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("************************************************************");
-        System.out.println("*                 CONVERSOR DE TEMPERATURA                 *");
-        System.out.println("************************************************************");
-        System.out.println("1. Celcius a Farenheit");
-        System.out.println("2. Farenheit a Celcius \n");
-        System.out.println("¿Qué operacion quieres realizar?");
-        operacion = sc.nextInt();
-        System.out.println("Dame los grados a convertir: ");
-        temperatura = sc.nextDouble();
-        switch(operacion){
-            
-            case 1:
-                Temperatura temp1 = new Temperatura(temperatura);
-                temp1.convertirCeliusAFarenheit();
-                System.out.println(temp1);
-                break;
-            case 2:
-                Temperatura temp2 = new Temperatura(temperatura);
-                temp2.convertirFarenheitACelcius();
-                System.out.println(temp2);
-                break;
-            default:
-                System.out.println("La operación no existe");
+        try{
+            do{
+                System.out.println("************************************************************");
+                System.out.println("*                 CONVERSOR DE TEMPERATURA                 *");
+                System.out.println("************************************************************");
+                System.out.println("Dame los grados a convertir: ");
+                temperatura = sc.nextDouble();
+                System.out.println("1. Celcius a Farenheit");
+                System.out.println("2. Farenheit a Celcius \n");
+                System.out.println("¿Que conversion quieres realizar?");
+                operacion = sc.nextInt();
+                
+                switch(operacion){
+
+                    case 1:
+                        Temperatura temp1 = new Temperatura(temperatura);
+                        temp1.convertirCeliusAFarenheit();
+                        System.out.println(temp1);
+                        break;
+                    case 2:
+                        Temperatura temp2 = new Temperatura(temperatura);
+                        temp2.convertirFarenheitACelcius();
+                        System.out.println(temp2);
+                        break;
+                    default:
+                        System.out.println("La operacion no existe");
+                        break;
+                }
+            }while(operacion == 1 || operacion == 2);
+        }catch(Exception e){
+            System.out.println("-------------------------------");
+            System.out.println("  Se requiere un numero entero ");
+            System.out.println("-------------------------------");
         }
     }
 }
